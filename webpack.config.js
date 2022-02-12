9e25dcb5d3f37611f217638d900008a7fbe41cca
matteo9966 <matteoluigitommasi@gmail.com>
2022-02-12 20:09:54 +0100

    aggiunto html-loader nel file di configurazione di webpack

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,10 @@ const config = {
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
-            },
+            },{
+                test:/\.(html)$/,
+                use:['html-loader'],
+            }
 
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/

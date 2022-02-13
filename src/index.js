920ac9186e435b4e64bbcadcbd9e9c26a7d64cb4
matteo9966 <matteoluigitommasi@gmail.com>
2022-02-13 10:12:27 +0100

    portfoliocard project finished!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import './styles/main.scss';
-import './sizeWidget/index.js';
-import twitter from './images/twitter-sign.png';
-import instagram from './images/instagram.png';
-import dribble from './images/dribbble-logo.png';
-import behance from './images/behance.png';
-console.log("Hello World!");
